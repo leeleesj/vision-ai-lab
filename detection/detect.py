2deed5fc59dda99368afc36d337699fbaa8df484
sangju.lee <sjleee.dev@gmail.com>
2024-08-09 18:07:55 +0900

    Add: YOLOv8 inference

--- detection/detect.py
+++ detection/detect.py
@@ -1,1 +1,8 @@
-import cv2
+from ultralytics import YOLO
+
+def load_model(model_path):
+    return YOLO(model_path)
+
+if __name__ == '__main__':
+    model = load_model("{YOUR_MODEL_PATH}")
+    model.predict("../assets/bus.jpg", save=True, imgsz=320, conf=0.5)
