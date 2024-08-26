7937844b9bc2763b489e1325ede77d9558f0bfb8
sangju.lee <sjleee.dev@gmail.com>
2024-08-26 22:37:52 +0900

    Feat: Video frame 추출 예외처리 추가

--- videoProcessing/video_frame_extractor.py
+++ videoProcessing/video_frame_extractor.py
@@ -1,34 +1,44 @@
 import os
 import cv2
 from glob import glob
+from tqdm import tqdm
 
 def extract_frames(video_path, output_folder):
-    video = cv2.VideoCapture(video_path)
+    try:
+        video = cv2.VideoCapture(video_path)
+        total_frames = int(video.get(cv2.CAP_PROP_FRAME_COUNT))
 
-    # initialize video frame count
-    count = 0
-    while True:
-        success, image = video.read()
-        if not success:
-            break
+        if total_frames == 0:
+            print(f"경고: {video_path}에서 프레임을 읽을 수 없습니다.")
+            return
 
-        cv2.imwrite(os.path.join(output_folder, f"frame_{count:04d}.jpg"), image)
-        count += 1
+        for count in tqdm(range(total_frames), desc="프레임 추출 중"):
+            success, image = video.read()
+            if not success:
+                print(f"경고: 프레임 {count}를 읽는 데 실패했습니다.")
+                break
 
-    video.release()
+            cv2.imwrite(os.path.join(output_folder, f"frame_{count:04d}.jpg"), image)
+
+    except Exception as e:
+        print(f"오류: {video_path} 처리 중 예외 발생 - {str(e)}")
+    finally:
+        video.release()
 
 def process_videos(input_folder):
     video_files = glob(os.path.join(input_folder, "*.*"))
 
     for video_file in video_files:
         video_name = os.path.splitext(os.path.basename(video_file))[0]
-
         output_folder = os.path.join(input_folder, video_name)
-        os.makedirs(output_folder, exist_ok=True)
+        try:
+            os.makedirs(output_folder, exist_ok=True)
+            extract_frames(video_file, output_folder)
+            print(f"Processed {video_name}")
+        except Exception as e:
+            print(f"error creating output folder: {e}")
 
         extract_frames(video_file, output_folder)
-
-        print(f"Processed {video_name}")
 
 
 if __name__ == "__main__":
